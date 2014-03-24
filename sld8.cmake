09559431f33d199d783f2bd3d67d86e3be38fa72
martindbenoit <martinb@bluestreaktech.com>
2014-03-24 14:56:02 -0400

    Added cmake script to build for x86 and sld8

---
+++ sld8.cmake
@@ -0,0 +1,6 @@
+SET(CMAKE_SYSTEM_NAME Linux)  # Tell CMake we're cross-compiling
+include(CMakeForceCompiler)
+# Prefix detection only works with compiler id "GNU"
+# CMake will look for prefixed g++, cpp, ld, etc. automatically
+CMAKE_FORCE_C_COMPILER(arm-none-linux-gnueabi-gcc GNU)
+#SET(ANDROID TRUE)
